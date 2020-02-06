4b225dd964a28a6978246523fa1f8284529b403d
Sam Judge <samuel.judge.92@gmail.com>
2020-02-06 21:46:53 +0000

    ContentContainer  removed

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,6 @@ import Nav from './Nav'
 import NavLink from './NavLink'
 import Logo from './Logo'
 import ContentPanel from './ContentPanel'
-import ContentContainer from './ContentContainer'
-import Mandala from './Mandala'
 import Quote from './Quote'
 import Text from './Text'
 
@@ -32,17 +30,17 @@ const App = () => {
                     <Text title="Meditation">
                         <p>
                             I have been meditating for over 20 years; it has been a journey full of amazing and magical experiences and so much evolution and learning. I feel so passionately about the gifts that Meditation brings and feel honoured to be able to offer these gifts forward. Sharing what works Well and some of the pitfalls, as well as great Tools and Techniques but most of all an doing so with the Humble Energy of Love.
-                                </p>
+                        </p>
                     </Text>
                     <Quote color='blue'>
                         <p>
                             As Meditation becomes more a part of who you are, moment to moment, it becomes effortless, joyous; something which you drop into. An opening of space… for higher wisdom, guidance and universal love to come through.
-                                </p>
+                        </p>
                     </Quote>
                     <Text>
                         <p>
                             If you would like to bring more focus to your Meditation Practice it can be really helpful to observe where you are currently and how you feel in relation to it, through asking questions. I have put a few examples which work well below:
-                                </p>
+                        </p>
                     </Text>
 
                 </ContentPanel>
